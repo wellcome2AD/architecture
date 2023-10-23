aeb568ea60a873b2eebb44803a97a5479ff8ea75
wellcome2AD <iuuyyttttt@mail.ru>
2023-10-23 13:27:17 +0300

    структура Message еренесена в отдельный файл Message/Message.h

--- Client/AppClientMain.cpp
+++ Client/AppClientMain.cpp
@@ -7,38 +7,6 @@
 #include "../helpers/UtilString.h"
 #include "../helpers/UtilFile.h"
 
-static std::string getFormat(const std::string& msg) {
-	std::string format;
-	if (fileExists(msg))
-	{
-		auto dot_befor_ext = msg.find_last_of('.');
-		auto extension = msg.substr(dot_befor_ext);
-		size_t ext_size = extension.size();
-		format = "file " + std::string((char*)(&ext_size), sizeof(size_t)) + extension;
-	}
-	else
-	{
-		format = "text ";
-	}
-	return format;
-}
-
-static std::string readFromFile(const std::string& fileName) {
-	std::string data;
-	if (fileExists(fileName)) {
-		std::ifstream file(fileName, std::ios::binary);
-		if (file.is_open())
-		{
-			data = std::string(std::istreambuf_iterator<char>(file), {});
-		}
-		else
-		{
-			printf("Error\n");
-		}
-	}
-	return data;
-}
-
 int main()//(int argc, char* argv[])
 {
 	/*if (argc < 5 || split(argv[3], ":").size() != 2)
@@ -58,30 +26,7 @@ int main()//(int argc, char* argv[])
 		msg += std::string(" ") + argv[i];
 	}
 
-	// format of message with authorization and some file data is: 
-	// userNameLnegth userName file <size_t length of extention><extention><file data>
-
-	// format of text message with authorization is:
-	// text <text that was input by user>
-
-	Message msg_to_send;
-	size_t username_len = user_name.size();
-	msg_to_send.username = std::string((char*)(&username_len), sizeof(size_t)) + user_name;
-	
-	size_t password_len = password.size();
-	msg_to_send.password = std::string((char*)(&password_len), sizeof(size_t)) + password;
-
-	msg_to_send.format = getFormat(msg);
-
-	auto temp = readFromFile(msg);
-	if (temp.size() != 0) {
-		msg_to_send.message = temp;
-	}
-	else
-	{
-		msg_to_send.message = msg;
-	}
-	
+	Message msg_to_send(user_name, password, msg);	
 	std::ostringstream str_stream;
 	str_stream << msg_to_send;
 	Client().send(address, str_stream.str());
--- Message/Message.h
+++ Message/Message.h
@@ -3,30 +3,95 @@
 #include <string>
 #include <iostream>
 
+#include "../helpers/UtilFile.h"
+
 class Message {
 public:
-	Message() = default;
-	Message(const char* data, int n)
+	Message(const std::string& username, const std::string& password, const std::string& message)
 	{
+
+		// format of message with authorization and some file data is: 
+		// <size_t userNameLength><userName><size_t passwordLength><password>file <size_t length of extention><extention><file data>
+
+		// format of text message with authorization is:
+		// <size_t userNameLength><userName><size_t passwordLength><password>text <text that was input by user>
+
+		size_t username_len = username.size();
+		this->username = std::string((char*)(&username_len), sizeof(size_t)) + username;
+
+		size_t password_len = password.size();
+		this->password = std::string((char*)(&password_len), sizeof(size_t)) + password;
+
+		format = getFormat(message);
+
+		auto temp = readFromFile(message);
+		if (temp.size() != 0) {
+			this->message = temp;
+		}
+		else
+		{
+			this->message = message;
+		}
+	}
+	static Message Parse(const char* data, int n)
+	{
+		Message result;
 		size_t cur_index = 0;
 		auto username_len = *((size_t*)(&data[cur_index]));
 		cur_index += sizeof(size_t);
-		username = std::string(&data[cur_index], username_len);
+		result.username = std::string(&data[cur_index], username_len);
 		cur_index += username_len;
 
 		auto password_len = *((size_t*)(&data[cur_index]));
 		cur_index += sizeof(size_t);
-		password = std::string(&data[cur_index], password_len);
+		result.password = std::string(&data[cur_index], password_len);
 		cur_index += password_len;
 
-		format = std::string(&data[cur_index], std::find(&data[cur_index], data + n, ' '));
+		result.format = std::string(&data[cur_index], std::find(&data[cur_index], data + n, ' '));
 
-		cur_index += format.size() + 1;
-		message = std::string(&data[cur_index]);
+		cur_index += result.format.size() + 1;
+		result.message = std::string(&data[cur_index]);
+
+		return result;
 	}
 
 public:
 	std::string username, password, format, message;
+
+private:
+	Message() = default;
+
+	std::string getFormat(const std::string& msg) {
+		std::string format;
+		if (fileExists(msg))
+		{
+			auto dot_befor_ext = msg.find_last_of('.');
+			auto extension = msg.substr(dot_befor_ext);
+			size_t ext_size = extension.size();
+			format = "file " + std::string((char*)(&ext_size), sizeof(size_t)) + extension;
+		}
+		else
+		{
+			format = "text ";
+		}
+		return format;
+	}
+
+	std::string readFromFile(const std::string& fileName) {
+		std::string data;
+		if (fileExists(fileName)) {
+			std::ifstream file(fileName, std::ios::binary);
+			if (file.is_open())
+			{
+				data = std::string(std::istreambuf_iterator<char>(file), {});
+			}
+			else
+			{
+				printf("Error\n");
+			}
+		}
+		return data;
+	}
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Message& m) {

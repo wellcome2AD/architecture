53a612b7ccf13a076ffe8eb79bf5ebb444e6260d
wellcome2AD <iuuyyttttt@mail.ru>
2023-11-09 20:18:19 +0300

    удалены неопределённые методы базового класса IMessage

--- Message/AuthorizedMessage.h
+++ Message/AuthorizedMessage.h
@@ -7,11 +7,8 @@ public:
 	AuthorizedMessage() = default;
 	AuthorizedMessage(const std::string& username, const std::string& password, const std::string& message);
 
-	virtual format GetFormat() const = 0;
 	virtual std::string GetMsg() const override;
 	virtual void SetMsg(const std::string& m) override;
-	virtual void Deserialize(Deserializer& d) = 0;
-	virtual void Serialize(Serializer& s) const = 0;
 
 	virtual std::string GetUsername() const;
 	virtual void SetUsername(const std::string& n);
